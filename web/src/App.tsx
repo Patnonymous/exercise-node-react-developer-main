ca915a42dbfa76402b15c97f75084e3597dcc214
Patrick_Williamson <patrickw9080@outlook.com>
2022-06-30 14:54:08 -0400

    Updated to use language filtering for the repo list.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { RepoList } from './components/RepoList';
+import { LanguageSelector } from './components/LanguageSelector';
+import { Repo } from './models/Repo';
 
+/**
+ * @author Anonymous
+ * @description Fetches the repos from the api and returns them.
+ * @returns {JSON} JSON array of repos.
+ */
 const fetchRepos = async () => {
   const res = await fetch('http://localhost:4000/repos');
   const reposJson = await res.json();
@@ -8,31 +15,68 @@ const fetchRepos = async () => {
 };
 
 export function App() {
+  // Init state.
   const [repos, setRepos] = React.useState([]);
   const [fetchErr, setFetchErr] = React.useState(false);
+  const [langs, setLangs] = React.useState<any[]>([]);
+  const [langFilter, setLangFilter] = React.useState('all');
 
+  /**
+   * @description Saves the repos to the state, and saves unique languages
+   * to the state.
+   */
   React.useEffect(() => {
     fetchRepos()
       .then((repoData) => {
-        console.log(repoData);
+        // Sort the Repos by date created.
+        // Reverse when doing setRepos.
         repoData.sort((a: any, b: any) => {
           return (
             new Date(a.created_at).getTime() - new Date(b.created_at).getTime()
           );
         });
-        console.log(repoData);
+
+        // Get the relevant languages then set state.
+        const uniqueLangs: string[] = [];
+        repoData.forEach((repo: any) => {
+          if (!uniqueLangs.includes(repo.language)) {
+            uniqueLangs.push(repo.language);
+          }
+        });
+        setLangs(uniqueLangs);
         setRepos(repoData.reverse());
       })
       .catch((err) => {
-        console.log(err);
         setFetchErr(true);
       });
   }, []);
 
+  /**
+   * @description If a different lang is selected, set it as the filter lang.
+   * Else if it's the same then turn off filtering by setting to "all".
+   * @param lang Language to set.
+   */
+  const langSelectorClicked = (lang: string) => {
+    if (lang === langFilter) {
+      setLangFilter('all');
+    } else {
+      setLangFilter(lang);
+    }
+  };
+
   return (
     <div>
       <h1>Silverorange Repos</h1>
-      <RepoList fetchError={fetchErr} reposArray={repos} />
+      <LanguageSelector
+        onLangClickHandler={langSelectorClicked}
+        languages={langs}
+      />
+      <RepoList
+        fetchError={fetchErr}
+        reposArray={repos.filter(
+          (repo: Repo) => langFilter === 'all' || repo.language === langFilter
+        )}
+      />
     </div>
   );
 }
