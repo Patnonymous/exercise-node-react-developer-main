36960e95023525b69ef567ed2bfe79956ccba2d5
Patrick_Williamson <patrickw9080@outlook.com>
2022-07-04 10:09:02 -0400

    Updated interface names according to prettier settings.

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,19 +1,19 @@
 export interface User {
-    login: string;
-    id: number;
-    avatar_url: string;
-    gravatar_id: string;
-    url: string;
-    html_url: string;
-    followers_url: string;
-    following_url: string;
-    gists_url: string;
-    starred_url: string;
-    subscriptions_url: string;
-    organizations_url: string;
-    repos_url: string;
-    events_url: string;
-    received_events_url: string;
-    type: string;
-    site_admin: boolean;
+  login: string;
+  id: number;
+  avatarUrl: string;
+  gravatarUrl: string;
+  url: string;
+  htmlUrl: string;
+  followersUrl: string;
+  followingUrl: string;
+  gistsUrl: string;
+  starredUrl: string;
+  subscriptionsUrl: string;
+  organizationsUrl: string;
+  reposUrl: string;
+  eventsUrl: string;
+  receivedEventsUrl: string;
+  type: string;
+  siteAdmin: boolean;
 }
