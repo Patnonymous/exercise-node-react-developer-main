e4939611030d9ff8cac488f62b7bdf782e175664
Patrick_Williamson <patrickw9080@outlook.com>
2022-07-04 10:09:47 -0400

    Formatted for linter.

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,9 +1,8 @@
-import { Router, Request, Response, response } from 'express';
+import { Router, Request, Response } from 'express';
 import { Repo } from '../models/Repo';
 import axios from 'axios';
 import { readdir, readFile } from 'node:fs/promises';
 import path from 'node:path';
-
 
 export const repos = Router();
 
@@ -14,18 +13,18 @@ export const repos = Router();
  */
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
-  res.header("Content-Type", 'application/json');
-  let reposData: Array<Repo> = [];
-
+  res.header('Content-Type', 'application/json');
+  const reposData: Repo[] = [];
 
   // Data from API.
-  const response = await axios.get('https://api.github.com/users/silverorange/repos');
+  const response = await axios.get(
+    'https://api.github.com/users/silverorange/repos'
+  );
   response.data.forEach((repo: Repo) => {
     if (repo.fork === false) {
       reposData.push(repo);
-    };
+    }
   });
-
 
   // Data from local folder.
   // "Assume this file can change while the service is running." - Assuming 1. the contents might be updated and 2. the file name
@@ -37,17 +36,15 @@ repos.get('/', async (_: Request, res: Response) => {
     // Json data only.
     if (path.extname(file) === '.json') {
       const fileData = await readFile(path.join('./data', file));
-      let parsedLocalData: Array<Repo> = JSON.parse(fileData.toString());
+      const parsedLocalData: Repo[] = JSON.parse(fileData.toString());
 
       parsedLocalData.forEach((repo: Repo) => {
         if (repo.fork === false) {
           reposData.push(repo);
-        };
+        }
       });
-
-    };
-
-  };
+    }
+  }
 
   res.status(200);
   res.json(reposData);
